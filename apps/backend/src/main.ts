bd7ab369ee6cbcb9bf075eba7e54eca3c142b3ac
Prateek <kprateek870@gmail.com>
2025-08-17 00:25:34 +0530

    Typescript errors in nestjs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
